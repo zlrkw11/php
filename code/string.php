a3a3a3940bf22cc236bf4d242ad65d06a08407d0
ZL <ray040406@gmail.com>
2025-06-25 00:54:19 +1200

    feat: quotes usage

--- code/string.php
+++ code/string.php
@@ -2,19 +2,21 @@
     $string_one = 'my email is jd@gmail.com';
     $string_two = 'my name is John Doe';
     echo $string_one . $string_two;
+    $name = "John Doe";
+
+    // double quote can include variables
+    echo "Hey my name is $name";
+
+    echo "John said \"Hello World\".";
+    echo 'John said "Hello World".'
 ?>
 
 <!DOCTYPE html>
 
 <html>
 <head>
-    <title>my first php file</title>
+    <title></title>
 </head>
 <body>
-    
-    <div>
-        <?php echo $name;?>
-        <?php echo AGE; ?>
-    </div>
 <body>
 </html>
