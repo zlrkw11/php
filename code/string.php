96bcef59e2f680ae937b7a5598542a5aa429d12e
ZL <ray040406@gmail.com>
2025-06-25 00:50:01 +1200

    feat: strings in php

---
+++ code/string.php
@@ -0,0 +1,20 @@
+<?php
+    $string_one = 'my email is jd@gmail.com';
+    $string_two = 'my name is John Doe';
+    echo $string_one . $string_two;
+?>
+
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title>my first php file</title>
+</head>
+<body>
+    
+    <div>
+        <?php echo $name;?>
+        <?php echo AGE; ?>
+    </div>
+<body>
+</html>
