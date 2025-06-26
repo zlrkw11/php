b38d1ac3e90c193ea02f74c8734a342a680ad761
ZL <ray040406@gmail.com>
2025-06-26 12:18:22 +1200

    feat: numbers in php

---
+++ code/number.php
@@ -0,0 +1,27 @@
+<?php
+    $radius = 25;
+    $pi = 3.14;
+    // basic operators - * / + - **
+    $echo $pi * $radius ** 2;
+
+    // order of operations
+    echo 2 * (1 + 7) / 3;
+
+    // increment and decrement operators
+    echo $radius++;
+    echo $radius--;
+
+    // shorthand operators
+    $age = 20;
+    $age += 10;
+?>
+
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+<body>
+</html>
