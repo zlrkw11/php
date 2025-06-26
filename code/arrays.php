844e6d4102a51f308fbf66ac1a165e0018269524
ZL <ray040406@gmail.com>
2025-06-26 12:26:42 +1200

    feat: php arrays

---
+++ code/arrays.php
@@ -0,0 +1,25 @@
+<?php
+    // indexed arrays
+    $peopleOne = ['John', 'Jane', 'Doe'];
+    $peopleTwo = array('John', 'Jane', 'Doe');
+    $numbers = [20, 30, 40, 50];
+    echo $peopleOne[0]; 
+    echo $peopleTwo[0];
+    
+    // whenever we echo something, we need a string
+    print_r($ages);
+
+    // associative arrays
+
+
+?>
+
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+<body>
+</html>
