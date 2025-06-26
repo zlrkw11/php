0aa460c9406b5e5be5601b2123dd79319b460f7e
ZL <ray040406@gmail.com>
2025-06-26 15:35:11 +1200

    feat: indexed array methods

--- code/arrays.php
+++ code/arrays.php
@@ -7,10 +7,20 @@
     echo $peopleTwo[0];
     
     // whenever we echo something, we need a string
-    print_r($ages);
+    print_r($numbers);
 
+    // add a new item to the end of an array 
+    $numbers[]=60;
+    array_push($numbers, 70)
+
+    // array length
+    echo count($numbers);
+
+    // merge arrays
+    $peopleThree = array_merge($peopleOne, $peopleTwo);
+    print_r($peopleThree)
     // associative arrays
-
+    
 
 ?>
 
