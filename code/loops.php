7dec187ececa9d3dd1dd5cd0f8318625a9585644
ZL <ray040406@gmail.com>
2025-06-28 12:29:35 +1200

    feat: for loop and foreach loop

---
+++ code/loops.php
@@ -0,0 +1,21 @@
+<?php
+    $fruits = ['apple', 'banana', 'cherry', 'pear'];
+    for($i = 0; $i < count($fruits); $i++){
+        echo $fruits[$i] . '<br/>';
+    }
+
+    foreach($fruits as $fruit){
+        echo $fruit . '<br/>';
+    }
+
+?>
+
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+<body>
+</html>
