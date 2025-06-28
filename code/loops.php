bfd7517edc3e66e381c1f01e02afb31f5b6ac167
ZL <ray040406@gmail.com>
2025-06-28 12:43:35 +1200

    feat: while loop

--- code/loops.php
+++ code/loops.php
@@ -8,6 +8,19 @@
         echo $fruit . '<br/>';
     }
 
+    // multidimensional & associative array 
+    $cars = [['brand'=>'toyota', 'model'=>'prius'], ['brand'=>'mitsubishi', 'model'=>'lancer'], ['brand'=>'ford', 'model'=>'mustang']];
+    foreach($cars as $car){
+        echo $car['brand'] . ' - ' . $car['model'] . '<br/>';
+    }
+
+    // initialize i outside
+    $x = 0;
+    while ($x < count($cars)){
+        echo $cars[$x]['brand'];
+        echo '<br/>';
+        $x++;
+    }
 ?>
 
 <!DOCTYPE html>
@@ -17,5 +30,10 @@
     <title></title>
 </head>
 <body>
-<body>
+    <h1>Cars</h1>
+    <?php foreach($cars as $car) { ?>
+        <h3><?php echo $car['brand']; ?></h3>
+        <p><?php echo $car['model']; ?></p>
+    <?php } ?>
+</body>
 </html>
