30abb3e9390cc976fbd2a5e8d17712e1b1561e63
ZL <ray040406@gmail.com>
2025-06-28 12:06:35 +1200

    feat: php arrays example

--- code/arrays.php
+++ code/arrays.php
@@ -11,18 +11,22 @@
 
     // add a new item to the end of an array 
     $numbers[]=60;
-    array_push($numbers, 70)
+    array_push($numbers, 70);
 
     // array length
     echo count($numbers);
 
     // merge arrays
     $peopleThree = array_merge($peopleOne, $peopleTwo);
-    print_r($peopleThree)
+    print_r($peopleThree);
 
     // associative arrays (key & value pairs)
     $cars = ['toyota'=>'prius', 'mitsubishi'=>'lancer', 'ford'=>'mustang'];
-    
+    echo $cars['toyota'];
+    print_r($cars);
+    $cars['toyota']= 'corolla'; // change value
+    print_r($cars);
+
 ?>
 
 <!DOCTYPE html>
